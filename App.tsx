09d17336721fbd77ecc1b31fa9253864d612ef43
Seogba <101324621+Seogba@users.noreply.github.com>
2022-08-29 23:56:04 +0900

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -8,72 +8,70 @@ function App() {
   //any: 타입 검사 오류를 피함(타입이 지정되어 있지 않은 값)
   //symbol:전역적으로 고유한 참조값을 생성. Symbol()함수를 통해 생성
 
-  const firstName = Symbol("name");
-  const secondName = Symbol("name");
-  const n1:BigInt = BigInt(30);
 
-  const [count , setCount] = useState(Symbol(5));
-  const [int , setInt] = useState(BigInt(5));
+  type fish = {swim : () => void};
+  type Bird = {fly:() => void};
+  type Human = {swim?: () => void; fly?: () => void};
 
-
-  interface User{
-    name:string;
-    id?:number;
-  }
-
-  const user:User = {
-    name: "박지성",
-  }
-
-  class UserAccount{
-    name:string;
-    id:number;
-
-    constructor(name:string , id:number){
-      this.name = name;
-      this.id = id;
-    }
-  }
-
-  interface torres{
-    value:number 
-  }
-
- 
-
-  class koke{
-    readonly value:number[] = [3,6,9];
-  }
-
-  const user2:User = new UserAccount("Gerrard" , 8);
-
-  type StringArray = Array<string>;
-  type NumberArray = Array<number>;
-  type ObjectWithNameArray = Array<{name:string}>;
-
-  function getFavoriteNumber():number {
-    return 66;
-  }
-
-  function getUsersOnlineMessege(numUsersOnline:number){
-    if(numUsersOnline){
-      return 'There are ${numUsersOnline} online now!'
-    }
-
-    return "Nobody's here"
-  }
-
-  type Fish = {swim:()=>void};
-  type Bird = {fly: () => void};
-
-  function move(animal: Fish | Bird){
+  function move(animal:fish | Bird | Human){
     if("swim" in animal){
-      return animal.swim();
+      return animal.swim;
     }
-
-    return animal.fly();
+    else{
+      return animal.fly;
+    }
   }
 
+
+
+  function logValue (x:Date | string){
+    if(x instanceof Date){
+      console.log(x.toString)
+    }
+    else{
+      console.log(x.toUpperCase());
+    }
+  }
+
+function example() {
+  let x : string | number | boolean;
+
+  x = Math.random() < 0.5;
+
+  console.log(x);
+
+  if(Math.random() < 0.5){
+    x = "hello";
+    console.log(x);
+  }
+
+  else{
+    x = 100;
+    console.log(x);
+  }
+
+  return x;
+}
+
+function isFish(pet:fish | Bird) : pet is fish{
+  return (pet as fish).swim !== undefined;//컴파일러에게 무언가를 유형으로 처리하도록 지시
+}
+
+
+interface Circle{
+  kind: "circle";
+  radius ?: number;
+
+}
+
+interface Square{
+  kind:"square";
+  sideLength : number;
+}
+
+//never: 존재해서는 안되는 상태
+
+type Shape = Circle | Square;
   return (
     <div className="App">
       <h1>gello world</h1>
